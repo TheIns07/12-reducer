b4a0438bcd4d2474ee5605b7bd7aea0df9053c03
Arturo Cervantes <jesus.cervantes207003@potros.itson.edu.mx>
2022-11-02 11:19:51 -0700

    useReducer completado

--- src/components/MisJuegos.js
+++ src/components/MisJuegos.js
@@ -19,16 +19,58 @@ export const MisJuegos = () => {
             titulo: e.target.titulo.value,
             descripcion: e.target.descripcion.value
         };
+
+        const action = {
+            type: "crear",
+            payload: juego
+        };
+
+        dispatch(action);
+
+        console.log(juegos)
+    }
+
+    const borrar = id => {
+        const action = {
+            type: "borrar",
+            payload: id
+        };
+
+        dispatch(action);
+    }
+
+    const editar = (e, id) => {
+        console.log(e.target.value + " Editar "+ id)
+
+        let juego = {
+            id,
+            titulo: e.target.value,
+            descripcion: e.target.value
+        };
+
+        const action = {
+            type: "editar",
+            payload: juego
+        };
+
+        dispatch(action);
+
+        console.log(juegos)
     }
 
   return (
     <div>
         <h1>Estos son mis videojuegos</h1>
-        <p>Numero de videojuegos: 15</p>
+        <p>Numero de videojuegos: {juegos.length}</p>
         <ul>
-            <li>Gta</li>
-            <li>Halo</li>
-            <li>Gow</li>
+            {juegos.map(juego => (
+                <li key = {juego.id}>{juego.titulo}
+                <button onClick={e => borrar(juego.id)}>X</button>
+                <input type = "text" onBlur = {e => editar(e, juego.id)}
+                                    onKeyPress = {e => {if (e.key === "Enter") editar(e, juego.id)}}/>
+                </li>
+               
+            ))}
         </ul>
         <h3>Agregar Juego</h3>
         <form onSubmit={conseguirDatos}>
